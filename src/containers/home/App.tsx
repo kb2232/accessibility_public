961cd4ab423583602cae0c27e6781964e062a662
kunle babatunde <kbabatunde@ets.org>
2021-04-11 09:26:06 -0400

    first commit

---
+++ src/containers/home/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = ():JSX.Element => {
+  return (
+    <div className='home_container'>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
